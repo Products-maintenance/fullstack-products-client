de7f82d9ca52b0cb45ac34b7894dfe7cd27e88c8
Jordi Romero <jordirofu@gmail.com>
2026-04-28 00:28:14 +0200

    h1 title size text reduced for narrow screens

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -6,7 +6,7 @@ export default function Layout() {
         <div className="overflow-hidden">
             <header className="bg-slate-800">
              <div className="mx-auto max-w-6xl px-4 py-6 sm:px-10 sm:py-10">
-                    <h1 className="text-xl sm:text-4xl font-extrabold text-white wrap-break-word">Administrador de Productos</h1>
+                    <h1 className="text-lg sm:text-2xl font-extrabold text-white wrap-break-word">Administrador de Productos</h1>
                 </div>
             </header>
                      <main className="mt-6 sm:mt-10 mx-auto max-w-6xl px-4 sm:px-10 py-6 sm:py-10 bg-white shadow">
